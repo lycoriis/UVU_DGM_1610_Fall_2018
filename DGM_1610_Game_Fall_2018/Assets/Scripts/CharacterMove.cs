6fe2046edc2c7f91c6a2341315fc0899856bb863
Alexis Larson <novanocturne@gmail.com>
2018-09-12 16:32:14 -0600

    Fixed Code Objects still don't show

--- DGM_1610_Game_Fall_2018/Assets/Scripts/CharacterMove.cs
+++ DGM_1610_Game_Fall_2018/Assets/Scripts/CharacterMove.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour {
 
+	// Player Movement Variables
 	public int MoveSpeed;
 	public float JumpHeight;
 
+	// Player Grounded Variables
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	private bool grounded;
+
 	// Use this for initialization
 	void Start () {
-		print("Hello World!")
+		print("Hello World!");
 	}
 	
 	// Update is called once per frame
