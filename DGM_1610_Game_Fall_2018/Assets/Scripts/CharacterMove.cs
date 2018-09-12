f5738da259103fac3d76859a6f8cd3f8a0eb157b
Alexis Larson <novanocturne@gmail.com>
2018-09-12 17:16:11 -0600

    Debugged Code! Obj still invisible!

--- DGM_1610_Game_Fall_2018/Assets/Scripts/CharacterMove.cs
+++ DGM_1610_Game_Fall_2018/Assets/Scripts/CharacterMove.cs
@@ -21,11 +21,21 @@ public class CharacterMove : MonoBehaviour {
 	
 
 	void FixedUpdate () {
-		Grounded  = Physics2D.OverlapCircle(GroundCheck.Position, GroundCheckRadius, WhatIsGround);
+		Grounded  = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, WhatIsGround);
 	}
 
 	// Update is called once per frame
 	void Update () {
+
+		//This code makes the character jump
+		if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
+				Jump();
+		}
 		
+		//This code makes the character move from side to side using A&D keys
+	}
+
+	public void Jump (){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
 	}
 }
