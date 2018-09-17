a083087a3ce0399e09efdd3d382ac4a65a941360
Alexis Larson <novanocturne@gmail.com>
2018-09-17 17:04:12 -0600

    Added Test Level With Placeholder Sprite

--- DGM_1610_Game_Fall_2018/Assets/Scripts/CharacterMove.cs
+++ DGM_1610_Game_Fall_2018/Assets/Scripts/CharacterMove.cs
@@ -33,6 +33,12 @@ public class CharacterMove : MonoBehaviour {
 		}
 		
 		//This code makes the character move from side to side using A&D keys
+		if(Input.GetKey (KeyCode.D)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		if(Input.GetKey (KeyCode.A)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
 	}
 
 	public void Jump (){
