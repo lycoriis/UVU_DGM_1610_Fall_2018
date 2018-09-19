3bd2be390e5899d985dc5c6caa6f30761c84bba2
Alexis Larson <novanocturne@gmail.com>
2018-09-19 16:26:50 -0600

    Added VarFunk

---
+++ DGM_1610_Game_Fall_2018/Assets/Scripts/VarFunk.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VarFunk : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	//3 variables
+	public int MoveSpeed = 5;
+	public transform GroundCheck;
+	private float GroundCheckRadius;
+	public layermask WhatIsGround;
+
+	//Function
+	Void GroundCheck (int GroundCheck) { };
+	int MoveSpeed (int MoveSpeed) { };
+	
+}
