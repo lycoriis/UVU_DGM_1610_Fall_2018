e739eaf22e4593a6c5e30436fa503a2a599ef9fa
Alexis Larson <novanocturne@gmail.com>
2018-12-03 18:56:56 -0700

    Need to add enable and disable ammo notated on script

--- DGM_1610_Game_Fall_2018/Assets/Scripts/AmmoManager.cs
+++ DGM_1610_Game_Fall_2018/Assets/Scripts/AmmoManager.cs
@@ -8,13 +8,11 @@ public class AmmoManager : MonoBehaviour {
 	public static int Ammo;
 
 	Text AmmoText;
-	public GameObject Projectile;
 
 	// Use this for initialization
 	void Start () {
 		AmmoText = GetComponent<Text>();
 		Ammo = 1;
-		Projectile = GameObject.Find("Projectile");
 	}
 	
 	// Update is called once per frame
@@ -22,12 +20,12 @@ public class AmmoManager : MonoBehaviour {
 		//If Ammo goes below 0 it stays at 0, need to figure out RESPAWN/Death
 		if (Ammo <= 0){
 			Ammo = 0;
-			// gameObject.SetActive(false);
+			// NEED TO DISABLE PROJECTILE SCRIPT HERE
 		}
 		
 		//Ammo renable when 1 or more
 		if (Ammo >= 1){
-		// gameObject.SetActive(true);
+			// NEED TO ENABLE PROJECTILE SCRIPT HERE
 		}
 		
 		//Ammo cannot go higher than 3
