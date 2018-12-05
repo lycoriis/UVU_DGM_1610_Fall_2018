e39235aabb7ea6968116837b1e05019053ae1d31
Alexis Larson <novanocturne@gmail.com>
2018-12-05 16:14:50 -0700

    Ammo is now working! Rearranging tutorial area for game level.

--- DGM_1610_Game_Fall_2018/Assets/Scripts/PlayerShoot.cs
+++ DGM_1610_Game_Fall_2018/Assets/Scripts/PlayerShoot.cs
@@ -7,18 +7,20 @@ public class PlayerShoot : MonoBehaviour {
 	// Shoot Variables
 	public Transform FirePoint;
 	public GameObject Projectile;
+	public AmmoManager AmmoManagerCall;
+	
 
 	// Use this for initialization
 	void Start () {
 		Projectile = Resources.Load("Prefab/ProjectileForFire") as GameObject;
-		//Projectile = GameObject.Find("Projectile");
-		
+		AmmoManagerCall = FindObjectOfType <AmmoManager>();		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.RightControl))
+		if(Input.GetKeyDown(KeyCode.RightControl) && AmmoManager.Ammo >=1){
 			Instantiate(Projectile,FirePoint.position, FirePoint.rotation);
-		
+			AmmoManager.Ammo --;
+		}
 	}
 }
